d2079c48a6eb684ee8f2452e7b31af0d77360c16
KmEnriquez <kentmathewenriquez@gmail.com>
2024-11-26 10:15:15 +0800

    header 100%

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const burgir = document.querySelector(".burgir__menu"); 
+const headernav = document.querySelector(".header__nav"); 
 burgir.addEventListener("click", () => {
     burgir.classList.toggle("open");// trns the burgir to x
+headernav.classList.toggle("open");// header nav appears
 })
